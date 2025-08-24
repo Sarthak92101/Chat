b577ecef3988eb29e41e0a5098ba8277e7ea8189
Sarthak Sharma <sarthaksharma25702@gmail.com>
2025-08-24 21:06:42 +0530

    Completed

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride=require("method-override")
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"));
 
 
 main().
@@ -59,6 +61,24 @@ app.get("/chats/:id/edit",async (req,res)=>{
   let{id}=req.params;
   let chat=await Chat.findById(id)
   res.render("edit",{chat});
+})
+
+//Update Route
+app.put("/chats/:id",async(req,res)=>{
+  let {id}=req.params;
+  let {msg :newmsg}=req.body;
+  let updatedchat=await Chat.findByIdAndUpdate(id,{msg:newmsg},{
+    runValidators:true, new:true
+  });
+  console.log(updatedchat); 
+  res.redirect("/chats")
+})
+
+app.delete("/chats/:id",async (req,res)=>{
+  let {id}=req.params; 
+  let DeletedChat= await Chat.findByIdAndDelete(id);
+  console.log(DeletedChat);
+  res.redirect("/chats");
 })
 app.get("/", (req, res) => {
   res.send("root is working")
